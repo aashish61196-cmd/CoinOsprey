012edafe15a44eac5657074a5e0b3b487b89232c
aashish61196-cmd <aashish61196@gmail.com>
2026-07-05 14:26:00 +0530

    Fix import paths for authController and auth middleware

--- middleware/routes/authRoutes.js
+++ middleware/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, logout, getMe } = require("../controllers/authController");
-const { protect } = require("../middleware/auth");
+const { register, login, logout, getMe } = require("../../Controller/authController");
+const { protect } = require("../auth");
 
 router.post("/register", register);
 router.post("/login", login);
