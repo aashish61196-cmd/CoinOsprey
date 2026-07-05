a1701ec4a09f8313d8bbe481857215bb780f3d84
aashish61196-cmd <aashish61196@gmail.com>
2026-07-05 13:51:59 +0530

    Add logger utility with timestamped log methods

---
+++ middleware/utils/logger.js
@@ -0,0 +1,16 @@
+function timestamp() {
+  return new Date().toISOString();
+}
+
+const logger = {
+  info: (...args) => console.log(`[INFO ${timestamp()}]`, ...args),
+  warn: (...args) => console.warn(`[WARN ${timestamp()}]`, ...args),
+  error: (...args) => console.error(`[ERROR ${timestamp()}]`, ...args),
+  debug: (...args) => {
+    if (process.env.NODE_ENV !== "production") {
+      console.debug(`[DEBUG ${timestamp()}]`, ...args);
+    }
+  },
+};
+
+module.exports = logger;
