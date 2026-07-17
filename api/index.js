2258889b8352629817e87d83463c6ce32b7d7e30
aashish61196-cmd <aashish61196@gmail.com>
2026-07-17 22:50:38 +0530

    Refactor API handler to use ES module syntax

--- api/index.js
+++ api/index.js
@@ -1,1 +1,7 @@
- module.exports = require('../backend/server.js');
+// api/index.js
+export default function handler(req, res) {
+  const rawPath = req.query.path;
+  const path = Array.isArray(rawPath) ? rawPath.join('/') : (rawPath || '');
+  req.url = '/' + path; // ab Express isko sahi route samjhega
+  return app(req, res);
+}
