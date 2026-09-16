81a876b331262709b522ef8df50895ee1280d13d
aashish61196-cmd <aashish61196@gmail.com>
2026-09-16 13:23:39 +0530

    Delete backend/routes directory

--- backend/routes/newsletter.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/newsletterController');
-const { protect, adminOnly } = require('../middleware/auth');
-
-router.post('/subscribe', ctrl.subscribe);
-router.get('/unsubscribe', ctrl.unsubscribe);
-router.get('/admin/subscribers', protect, adminOnly, ctrl.listSubscribers);
-
-module.exports = router;
